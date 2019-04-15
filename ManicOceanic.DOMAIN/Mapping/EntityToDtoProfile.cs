6d39463873fc2836fabafcc51075aeacafbfd106
Jonathanmoller <43174289+Jonathanmoller@users.noreply.github.com>
2019-04-15 10:55:56 +0200

    AddEnumExtensions

--- ManicOceanic.DOMAIN/Mapping/EntityToDtoProfile.cs
+++ ManicOceanic.DOMAIN/Mapping/EntityToDtoProfile.cs
@@ -3,6 +3,7 @@ using ManicOceanic.DOMAIN.Dto;
 using ManicOceanic.DOMAIN.Entities;
 using ManicOceanic.DOMAIN.Entities.Products;
 using ManicOceanic.DOMAIN.Entities.Sales;
+using ManicOceanic.DOMAIN.Extensions;
 
 namespace ManicOceanic.DOMAIN.Mapping
 {
@@ -14,7 +15,7 @@ namespace ManicOceanic.DOMAIN.Mapping
       CreateMap<Customer, CustomerDto>();
       CreateMap<Order, OrderDto>();
       CreateMap<Product, ProductDto>().ForMember(src => src.UnitOfMeasure,
-        opt => opt.MapFrom(src => src.UnitOfMeasure.ToString()));
+        opt => opt.MapFrom(src => src.UnitOfMeasure.ToDescriptionString()));
     }
   }
 }
