bfd3acb4f29e37b8e6d75ec772afeea0959b839b
Jonathanmoller <43174289+Jonathanmoller@users.noreply.github.com>
2019-04-24 12:53:51 +0200

    Remove faulty using statement

--- ManicOceanic.WEB/Startup.cs
+++ ManicOceanic.WEB/Startup.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ManicOceanic.WEB.Data;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ManicOceanic.WEB
 {
-  public class Startup
+    public class Startup
   {
     public Startup(IConfiguration configuration)
     {
