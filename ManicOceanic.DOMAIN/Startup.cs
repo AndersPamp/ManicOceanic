d0dce65f75796b5d71f6e97547ccdac93c102488
Jonathanmoller <43174289+Jonathanmoller@users.noreply.github.com>
2019-04-29 10:21:19 +0200

    Resolved a bug in startup

--- ManicOceanic.DOMAIN/Startup.cs
+++ ManicOceanic.DOMAIN/Startup.cs
@@ -16,48 +16,52 @@ using Microsoft.Extensions.DependencyInjection;
 namespace ManicOceanic.DOMAIN
 {
     public class Startup
-  {
+    {
         public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration; 
+            Configuration = configuration;
         }
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
-    {
 
-        services.AddScoped<IOrderService, OrderService>();
-        services.AddScoped<IProductService, ProductService>();
-        services.AddScoped<ICustomerService, CustomerService>();
-        services.AddScoped<ICategoryService, CategoryService>();
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
 
-        services.AddScoped<ICategoryRepository, CategoryRepository>();
-        services.AddScoped<IProductRepository, ProductRepository>();
-        services.AddScoped<ICustomerRepository, CustomerRepository>();
-        services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-        services.AddAutoMapper(); //for mapping dto to models etc
-        }
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.AddAutoMapper();
+
 
             services.AddDbContext<MOContext>(options => options.UseSqlServer(
                 Configuration.GetConnectionString("DefaultConnection")));
-    }
-       
+        }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
 
-      //app.Run(async (context) =>
-      //{
-      //  await context.Response.WriteAsync("Hello World!");
-      //});
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Run(async (context) =>
+            //{
+            //  await context.Response.WriteAsync("Hello World!");
+            //});
+        }
     }
-  }
 }
+
