86f43dda4275f8943dab8247bcfec2663c88ae32
Jonathanmoller <43174289+Jonathanmoller@users.noreply.github.com>
2019-05-02 15:19:07 +0200

    Add Product Cart html

---
+++ ManicOceanic.WEB/Controllers/ProductCartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManicOceanic.WEB.Controllers
+{
+    public class ProductCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
