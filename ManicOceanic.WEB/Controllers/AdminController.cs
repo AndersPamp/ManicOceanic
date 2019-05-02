f642a0aada982e63fa7d14945173609828a043b5
Jonathanmoller <43174289+Jonathanmoller@users.noreply.github.com>
2019-05-02 16:03:03 +0200

    Add admin page

---
+++ ManicOceanic.WEB/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManicOceanic.WEB.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
