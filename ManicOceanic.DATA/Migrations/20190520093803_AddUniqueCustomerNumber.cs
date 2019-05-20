cbf0c710e29303cd9151d57ddfbdbde9e5dddc50
Lars Bylund <lars.bylund@hotmail.com>
2019-05-20 11:41:46 +0200

    AddNewMigration

---
+++ ManicOceanic.DATA/Migrations/20190520093803_AddUniqueCustomerNumber.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManicOceanic.DATA.Migrations
+{
+    public partial class AddUniqueCustomerNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_AspNetUsers_CustomerNumber",
+                table: "AspNetUsers",
+                column: "CustomerNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_AspNetUsers_CustomerNumber",
+                table: "AspNetUsers");
+        }
+    }
+}
