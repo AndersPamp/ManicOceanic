b4861a23967bc125e739a731c6ac7045cda41977
evapersson <persson_eva@outlook.com>
2019-05-23 10:23:16 +0200

    DetailPage for products done

---
+++ ManicOceanic.WEB/Models/ProductDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManicOceanic.DOMAIN.Entities.Products;
+using ManicOceanic.WEB.Dto;
+
+namespace ManicOceanic.WEB.Models
+{
+    public class ProductDetailViewModel
+    {
+        public Product Product { get; set; }
+
+    }
+}
